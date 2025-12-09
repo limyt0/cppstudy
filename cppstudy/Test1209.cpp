5923b305bfbef434d1fcb59e69ff19e05409e3c7
limyt0 <dudx45@gmail.com>
2025-12-09 12:57:41 +0900

    Test1208 09

--- cppstudy/Test1209.cpp
+++ cppstudy/Test1209.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main() {
-    
+
     int arr[3] = { 100,200,300 };
-    
+
     int* p = arr;
 
     int(&arr2)[3] = arr;
 
 
-    
+
     cout << "p = " << p << "(arr[0] 林家)" << endl;
-    cout << "p + 1 =" << p+1 << "(arr[1] 林家)" << endl;
+    cout << "p + 1 =" << p + 1 << "(arr[1] 林家)" << endl;
     cout << "p + 2 =" << p + 2 << "(arr[2] 林家)" << endl;
 
-    cout << "arr2 = " << arr2[0] <<  endl;
-    cout << "arr2 + 1 =" << *(arr2+1) << endl;
+    cout << "arr2 = " << arr2[0] << endl;
+    cout << "arr2 + 1 =" << *(arr2 + 1) << endl;
     cout << "arr2 + 2 =" << arr2[2] << endl;
 
     cout << "*(p) = " << p[0] << endl;
-    cout << "*(p+1) = " << *(p+1) << endl;
-    cout << "*(p+21) = " <<  p[2] << endl;
+    cout << "*(p+1) = " << *(p + 1) << endl;
+    cout << "*(p+21) = " << p[2] << endl;
     return 0;
 }
