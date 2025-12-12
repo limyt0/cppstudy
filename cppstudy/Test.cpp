99ab3e3d2201a1f8341aaf349de5b811d8f6ca1c
limyt0 <dudx45@gmail.com>
2025-12-12 16:30:33 +0900

    Test1212

--- cppstudy/Test.cpp
+++ cppstudy/Test.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void SwapByPointer(int* pA, int* pB) {
-    int temp;
-    temp = *pA;
-    *pA = *pB;
-    *pB = temp;
-}
-void SwapByReference(int& refA, int& refB) {
-    int temp;
-    temp = refA;
-    refA = refB;
-    refB = temp;
-}
-
 int main() {
-    int Ap = 1;
-    int Bp = 2;
-    int Ar = 3;
-    int Br = 4;
-    SwapByPointer(&Ap, &Bp);
-    cout << Ap << ", " << Bp << endl;
-    SwapByReference(Ar, Br);
-    cout << Ar << ", " << Br << endl;
+    int arr[3] = { 100,200,300 };
 
+    int* arr2 = arr;
+    //auto arr2 = arr; //이렇게 auto 사용해도 됨.
+
+    cout << "arr2 = " << arr2[0] << endl;
+    cout << "arr2 + 1 =" << *(arr2 + 1) << endl;
+    cout << "arr2 + 2 =" << arr2[2] << endl;
     return 0;
 }
-
