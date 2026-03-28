90fabc81565c134cc637b818e168454017122149
limyt0 <dudx45@gmail.com>
2026-03-28 21:32:10 +0900

    test

--- cppstudy/Test.cpp
+++ cppstudy/Test.cpp
@@ -1,43 +1,60 @@
-#include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
-//#include <unordered_map>
-#include <set>
-#include <sstream>
+#include <map>
 
 using namespace std;
 
-string solution(string s) {
-    string answer = "";
-    string str;
-    stringstream ss(s);
-    int max = 0;
-    int min = 0;
-    bool isfirst = true;
-    while (ss >> str)
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+bool search_bst(Node* cur, int target)
+{
+    if (cur == nullptr) return false;
+
+    if (cur->data == target) return true;
+    else if (target < cur->data) 
     {
-        int a = stoi(str);
-        if (isfirst)
-        {
-            min = a;
-            max = a;
-            isfirst = false;
-        }
-        if (a > max) max = a;
-        if (a < min) min = a;
+        cout << "L " << endl;
+        return search_bst(cur->left, target);
     }
-    answer = to_string(min) + " " + to_string(max);
-    return answer;
+    else
+    {
+        cout << "R " << endl;
+        return search_bst(cur->right, target);
+    }
+}
+
+Node* insert_bst(Node* root, int val)
+{
+    if (root == nullptr) {
+        return new Node(val);
+    }
+
+    if (val < root->data) {
+        root->left = insert_bst(root->left, val);
+    }
+    else {
+        root->right = insert_bst(root->right, val);
+    }
+    return root;
 }
 
 int main() {
-    string s = "1 2 3 4";
-    
+    int N = 7;
+    vector<int> nums = { 3, 4, 2, 8, 9, 7, 1 };
+    int target = 7;
+    Node* root = new Node(3);
+    for (int i : nums)
+    {
+        insert_bst(root, i);
+    }
+
+    search_bst(root, target);
 
 
-    cout << solution(s) << endl;
-    
     return 0;
 }
